730e9678d2d84b9712844e5990c29d1d2ddc00e9
GabrielSousaJS <gabriels.santosx@gmail.coom>
2023-06-07 11:25:08 -0300

    Adicionando rotas para os detalhes de cada Pokemon

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,8 @@ import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
 
+import {RouterModule} from "@angular/router";
+
 
 
 @NgModule({
@@ -20,7 +22,8 @@ import { PokeListComponent } from './poke-list/poke-list.component';
     PokeListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
